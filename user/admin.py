b8eaf2cd3e2fed6b4c0c557d83347ed9e40dfad3
Gammbity <abduboriyabdusamadov66@gmail.com>
2024-09-30 16:33:53 +0500

    feat(user) User and (common) BaseModel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from user import models
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+@admin.register(models.User)
+class UserModelAdmin(UserAdmin):
+    ordering = ('email',)
+    list_display = ['first_name', 'email']
+    list_display_links =['first_name', 'email']
+    search_fields = ("email", "first_name")
+    readonly_fields = ['created_at', 'updated_at']
+    
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "phone")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+from common.models import BaseModel
+
+class User(AbstractUser, BaseModel):
+    password = models.CharField(max_length=20)
+    phone = models.CharField(max_length=13)
+
+    def __str__(self) -> str:
+        return self.get_full_name()
+
+    class Meta:
+        verbose_name = _("Foydalanuvchi")
+        verbose_name_plural = _("Foydalanuvchilar")
+
