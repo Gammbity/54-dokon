3fb45152edc5011b89534294b6f9f85381906e55
Gammbity <abduboriyabdusamadov66@gmail.com>
2024-10-03 16:44:12 +0500

    feat(product) Product and Category models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from common.models import BaseModel
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+class Category(BaseModel):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = _("kategoriya")
+        verbose_name_plural = _("kategoriyalar")
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    real_price = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    description = RichTextUploadingField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='product')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = _("mahsulot")
+        verbose_name_plural = _("mahsulotlar")
