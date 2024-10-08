49846b29337829562b211d57353715b858672ca8
Gammbity <abduboriyabdusamadov66@gmail.com>
2024-10-08 20:33:44 +0500

    feat(product)products and product test done

--- product/tests.py
+++ product/tests.py
@@ -2,13 +2,20 @@ from django.test import TestCase
 from product import models
 
 
-# class ProductTestCase(TestCase):
+class ProductTestCase(TestCase):
 
-#     def setUp(self):
-#         self.category = models.Category.objects.create(name="test_C")
-#         models.Product.objects.create(name="test1_P", price="12000$", real_price="10000$", description="test_description", category=self.category)
-#         models.Product.objects.create(name="test2_P", price="12000$", real_price="10000$", description="test_description", category=self.category)
+    def setUp(self):
+        self.category = models.Category.objects.create(name="test_C")
+        models.Product.objects.create(name="test1_P", price="12000$", real_price="10000$", description="test_description", category=self.category)
+        models.Product.objects.create(name="test2_P", price="12000$", real_price="10000$", description="test_description", category=self.category)
 
-#     def test_list_products(self):
-#         response = self.client.get("/api/v1/product/products/")
-#         print(response.data)
+    def test_list_products(self):
+        response = self.client.get("/api/v1/product/products/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("test1_P", response.data[0]['name'])
+        self.assertIn("test2_P", response.data[1]['name'])
+
+    def test_get_product(self):
+        response = self.client.get(f"/api/v1/product/product/{2}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("test2_P", response.data['name'])
--- user/tests.py
+++ user/tests.py
@@ -2,29 +2,30 @@ from django.test import TestCase
 from user.models import User
 
 
-class MeViewTest(TestCase):
+# class MeViewTest(TestCase):
 
-    def test_get_user(self):
-        self.user = User.objects.create_user(
-            first_name='Ali',
-            last_name='Aliyev',
-            username='testuser',
-            email='test@gmail.com',
-            phone='+998880334626',
-            password='testpassword'
-        )
-        self.client.login(username="testuser", password="testpassword")
-        response = self.client.get(f'/api/v1/user/me/')  
-        self.assertEqual(response.status_code, 200)
+#     def test_get_user(self):
+#         self.user = User.objects.create_user(
+#             first_name='Ali',
+#             last_name='Aliyev',
+#             username='testuser',
+#             email='test@gmail.com',
+#             phone='+998880334626',
+#             password='testpassword'
+#         )
+#         self.client.login(username="testuser", password="testpassword")
+#         response = self.client.get(f'/api/v1/user/me/')  
+#         self.assertEqual(response.status_code, 200)
 
-    def test_registr_user(self):
-        user = {
-            "first_name":"Ali",
-            "last_name":"Aliyev",
-            "username":"testuser",
-            "email":"test@gmail.com",
-            "phone":"+998880334626",
-            "password":"Qwerty1234root"
-        }
-        respone = self.client.post("/api/v1/user/registration/", data=user)
-        self.assertEqual(respone.status_code, 201)
+#     def test_registr_user(self):
+#         user = {
+#             "first_name":"Ali",
+#             "last_name":"Aliyev",
+#             "username":"testuser",
+#             "email":"test@gmail.com",
+#             "phone":"+998880334626",
+#             "password":"Qwerty1234root"
+#         }
+#         respone = self.client.post("/api/v1/user/registration/", data=user)
+#         self.assertEqual(respone.status_code, 201)
+
