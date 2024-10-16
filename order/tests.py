239f1ad7d5779d19ed4cad299914406e014bfa32
Gammbity <abduboriyabdusamadov66@gmail.com>
2024-10-16 17:02:18 +0500

    feat(order) basket's tests added

--- order/tests.py
+++ order/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from order.models import Order
+from order.models import Order, Basket
 from user.models import User
 from product.models import Product, Category
 
@@ -50,5 +50,45 @@ class OrderTestCase(TestCase):
 
         response = self.client.post("/api/v1/order/order/create/", data=order)
         self.assertEqual(response.status_code, 201)
-        self.assertIn('user', response.data)
+        self.assertIn('product', response.data)
+
+
+class BasketTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(
+            first_name='Ali',
+            last_name='Aliyev',
+            username='testuser',
+            email='test@gmail.com',
+            phone='+998880334626',
+            password='testpassword'
+        )
+        self.client.login(username="testuser", password="testpassword")
+        self.category = Category.objects.create(name="test_C")
+        self.product = Product.objects.create(
+            name="test_P", 
+            price=12000, 
+            real_price="10000$", 
+            description="test_description", 
+            category=self.category
+        )
+        
+    def test_basket_get(self):
+        Basket.objects.create(
+            user=self.user, 
+            product=self.product, 
+            )
+        
+        response = self.client.get("/api/v1/order/basket/")
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("test_P", response.data[0]['product']['name'])
+    
+    def test_basket_create(self):
+        basket = {
+            'user':self.user.id,
+            'product':self.product.id
+        }
+        response = self.client.post("/api/v1/order/basket/create/", data=basket)
+        self.assertEqual(response.status_code, 201)
         self.assertIn('product', response.data)
