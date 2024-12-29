28f42f47e2970f80052190623abac083a2c250df
Gammbity <abduboriyabdusamadov33@gmail.com>
2024-12-29 18:13:34 +0500

    send message more than 1 tg user when order created

--- bot/order_bot.py
+++ bot/order_bot.py
@@ -3,6 +3,8 @@ from asyncio import run
 
 dp = Dispatcher()
 
+tg_id = [6089066974]
+
 bot = Bot('7707356933:AAF36G6W9l_ntaKkXRrQpU4n_65gpvReY_4')
 
 async def start():
@@ -13,7 +15,7 @@ if __name__ == "__main__":
 
 async def bot_order(order_user, bot):
     try:
-        # Your async telegram bot logic here
-        await bot.send_message(6089066974, f"Order User: {order_user}")
+        for i in tg_id:
+            await bot.send_message(i, f"Order User: {order_user}")
     except Exception as e:
         print(f"Error sending telegram message: {e}")
